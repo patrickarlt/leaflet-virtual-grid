64f7e68890906ef15fcc54d762c74e5f3177a9e9
Patrick Arlt <patrick.arlt@gmail.com>
2013-07-31 09:30:17 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Leaflet Virtual Grid
+
+You can use `L.VirtualGrid` to generate simple, cacheable, grids of `L.LatLngBounds` objects you can use to query and API.
+
+This lets you query APIs for smaller units and space and never make a call data in the same area twice.
+
+# Example
+
+Here is what the grid looks like under the hood...
+
+![Example](https://raw.github.com/Esri/esri-leaflet/master/esri-leaflet.png)
+
+Each rectangle would represent a call to an API or query to a data source. You would only make one request per cell so you not make repeat calls to areas like requesting all the data in a map view when a user performs a small pan.
+
+Currently the grid is rebuild from scratch when a user zooms in or out Eventually the goal is to have `L.VirtualGrid` automatically repack grid cells when users zoom in or out so avoid repeat calls even across zoom levels.
+
+# To Do
+* **Repack grid cells across zoom levels**
+* Cross browser testing
+* Building system
+* Unit tests
+* Documentation
---
+++ src/virtual-grid.js
@@ -0,0 +1,126 @@
+(function(L){
+  function debounce(fn, delay, context) {
+    var timer = null;
+    return function() {
+      var context = this||context, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, delay);
+    };
+  }
+
+  function roundAwayFromZero(num){
+    return (num > 0) ? Math.ceil(num) : Math.floor(num);
+  }
+
+  L.VirtualGrid = L.Class.extend({
+    includes: L.Mixin.Events,
+    options: {
+      cellSize: 512,
+      debounce: 100,
+      deduplicate: true
+    },
+    _previousCells: [],
+    initialize: function(options) {
+      L.Util.setOptions(this, options);
+    },
+    onAdd: function(map){
+      this._map = map;
+      this.center = this._map.getCenter();
+      this.origin = this._map.project(this.center);
+
+      this.handler = debounce(function(e){
+        if(e.type === "zoomend"){
+          this.origin = this._map.project(this.center);
+          this._previousCells = [];
+          this.fireEvent("clearcells");
+        }
+        this.fireEvent("newcells", this.cellsWithin(e.target.getBounds()));
+      }, this.options.debounce, this);
+
+      map.on("zoomend resize move", this.handler, this);
+
+      this.fireEvent("newcells", this.cellsWithin(this._map.getBounds()));
+    },
+    onRemove: function(map){
+      map.off("move zoomend resize", this.handler, this);
+    },
+    cellsWithin: function(mapBounds){
+      console.time("cellsWithin");
+      var size = this._map.getSize();
+      var offset = this._map.project(this._map.getCenter());
+      var bounds = mapBounds.pad(Math.min(this.options.cellSize/size.x, this.options.cellSize/size.y));
+      var cellInfo = {
+        bounds: bounds,
+        cells: []
+      };
+
+      var topLeftPoint = this._map.project(bounds.getNorthWest());
+      var bottomRightPoint = this._map.project(bounds.getSouthEast());
+
+      var topLeft = topLeftPoint.subtract(offset).divideBy(this.options.cellSize);
+      var bottomRight = bottomRightPoint.subtract(offset).divideBy(this.options.cellSize);
+
+      var offsetRows = Math.round((this.origin.x - offset.x) / this.options.cellSize);
+      var offsetCols = Math.round((this.origin.y - offset.y) / this.options.cellSize);
+
+      var minRow = roundAwayFromZero(topLeft.x)-offsetRows;
+      var maxRow = roundAwayFromZero(bottomRight.x)-offsetRows;
+      var minCol = roundAwayFromZero(topLeft.y)-offsetCols;
+      var maxCol = roundAwayFromZero(bottomRight.y)-offsetCols;
+
+      for (var row = minRow; row < maxRow; row++) {
+        for (var col = minCol; col < maxCol; col++) {
+          var cellId = "cell:"+row+":"+col;
+          var duplicate = this._previousCells.indexOf(cellId) >= 0;
+
+          if(!duplicate || !this.options.deduplicate){
+            var cellBounds = this.cellExtent(row, col);
+            var cellCenter = cellBounds.getCenter();
+            var radius = cellCenter.distanceTo(cellBounds.getNorthWest());
+            var distance = cellCenter.distanceTo(this.center);
+            var cell = {
+              row: row,
+              col: col,
+              id: cellId,
+              center: cellCenter,
+              bounds: cellBounds,
+              distance:distance,
+              radius: radius
+            };
+            cellInfo.cells.push(cell);
+            this._previousCells.push(cellId);
+          }
+        }
+      }
+
+      cellInfo.cells.sort(function (a, b) {
+        return a.distance - b.distance;
+      });
+      console.timeEnd("cellsWithin");
+      return cellInfo;
+    },
+    cellExtent: function(row, col){
+      var swPoint = this.cellPoint(row, col);
+      var nePoint = this.cellPoint(row+1, col+1);
+      var sw = this._map.unproject(swPoint);
+      var ne = this._map.unproject(nePoint);
+      return L.latLngBounds(sw, ne);
+    },
+    cellPoint:function(row, col){
+      var x = this.origin.x + (row*this.options.cellSize);
+      var y = this.origin.y + (col*this.options.cellSize);
+      return [x, y];
+    },
+    addTo: function (map) {
+      map.addLayer(this);
+      return this;
+    }
+  });
+
+  L.virtualGrid = function(options){
+    return new L.VirtualGrid(options);
+  };
+
+})(L);
